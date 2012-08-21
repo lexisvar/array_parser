836bd604073706f21e2951c8764fffbec2c7751f
lexisvar <alexis25615@gmail.com>
2012-08-21 12:54:33 -0500

    second

--- parser.py
+++ parser.py
@@ -1,1 +1,32 @@
-print "Hello"
+import sys
+lines = open(sys.argv[1]) 
+def s0():
+	print "state s0"
+def s1():
+	print "state s1"
+def s2():
+	print "state s2"
+def s3():
+	print "state s3"
+def s4():
+	print "state s4"
+def s5():
+	print "state s5"
+def s6():
+	print "state s6"
+def sE():
+	print "state sE"
+def states(x):
+	return{
+		's0':s0,
+		's1':s1,
+		's2':s2,
+		's3':s3,
+		's4':s4,
+		's5':s5,
+		's6':s6,
+		'sE':sE,
+	}[x]()
+for line in lines:
+	for character in line:
+		print character
