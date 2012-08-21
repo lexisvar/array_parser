0f5de3c467e596a20643c2c462d6dd86b9accf05
lexisvar <alexis25615@gmail.com>
2012-08-21 13:58:26 -0500

    third

--- parser.py
+++ parser.py
@@ -1,20 +1,21 @@
 import sys
-lines = open(sys.argv[1]) 
-def s0():
-	print "state s0"
-def s1():
+import re
+lines = open(sys.argv[1])
+def s0(char):
+	return True if char==('(a-Z)') else False
+def s1(char):
 	print "state s1"
-def s2():
+def s2(char):
 	print "state s2"
-def s3():
+def s3(char):
 	print "state s3"
-def s4():
+def s4(char):
 	print "state s4"
-def s5():
+def s5(char):
 	print "state s5"
-def s6():
+def s6(char):
 	print "state s6"
-def sE():
+def sE(char):
 	print "state sE"
 def states(x):
 	return{
@@ -29,4 +30,4 @@ def states(x):
 	}[x]()
 for line in lines:
 	for character in line:
-		print character
+		print s0(character)
