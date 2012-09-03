560af1e863ec468d00511053238b84a4895393b7
Alexis <alexis25615@gmail.com>
2012-09-02 23:13:11 -0500

    Modify

--- parser.py
+++ parser.py
@@ -2,32 +2,32 @@ import sys
 import re
 lines = open(sys.argv[1])
 def s0(char):
-	return True if char==('(a-Z)') else False
+        return True if re.match('([a-zA-Z])',char) else False
 def s1(char):
-	print "state s1"
+        print "state s1"
 def s2(char):
-	print "state s2"
+        print "state s2"
 def s3(char):
-	print "state s3"
+        print "state s3"
 def s4(char):
-	print "state s4"
+        print "state s4"
 def s5(char):
-	print "state s5"
+        print "state s5"
 def s6(char):
-	print "state s6"
+        print "state s6"
 def sE(char):
-	print "state sE"
+        print "state sE"
 def states(x):
-	return{
-		's0':s0,
-		's1':s1,
-		's2':s2,
-		's3':s3,
-		's4':s4,
-		's5':s5,
-		's6':s6,
-		'sE':sE,
-	}[x]()
+        return{
+                's0':s0,
+                's1':s1,
+                's2':s2,
+                's3':s3,
+                's4':s4,
+                's5':s5,
+                's6':s6,
+                'sE':sE,
+        }[x]()
 for line in lines:
-	for character in line:
-		print s0(character)
+        for character in line:
+                print character,s0(character)
